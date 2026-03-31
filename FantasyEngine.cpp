f0d00bc0febe726e360fe2e7275a0caadc44822c
wobey96 <wobey96@gmail.com>
2026-03-30 20:33:38 -0600

    Add project files.

---
+++ FantasyEngine.cpp
@@ -0,0 +1,12 @@
+﻿// FantasyEngine.cpp : Defines the entry point for the application.
+//
+
+#include "FantasyEngine.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake. From Fantaasy Engine" << endl;
+	return 0;
+}
