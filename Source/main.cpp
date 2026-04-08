844e0e91d1ad194362718807e27ce0d22ad2c995
wobey96 <wobey96@gmail.com>
2026-04-08 13:25:25 -0600

    Now the window is working

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,8 +1,24 @@
-﻿
-#include <iostream>
+﻿#define GLFW_INCLUDE_NONE 
+#include <GLFW/glfw3.h>
+#define GLFW_EXPOSE_NATIVE_WIN32
+#include <GLFW/glfw3native.h>
 
 int main()
 {
-	std::cout << "Hello World" << std::endl; 
+	if (!glfwInit())
+	{
+		abort(); 
+	}
+
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+	glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "FantasyEngine", nullptr, nullptr); 
+
+	while(!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents(); 
+	}
+	glfwTerminate(); 
 	return 0;
 }
